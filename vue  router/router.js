5ee92a1a89e3197eb18dd2fbfbf47a63d8e92c6c
csj9859 <1594258541@qq.com>
2019-07-12 19:03:43 +0800

    vue

---
+++ vue  router/router.js
@@ -0,0 +1,51 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import Home from './views/Home.vue'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: Home
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/news',
+      name: 'news',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "about" */ './views/News.vue'),
+      children:[
+        {
+          path: 'list',
+          name: 'list',
+          // route level code-splitting
+          // this generates a separate chunk (about.[hash].js) for this route
+          // which is lazy-loaded when the route is visited.
+          component: () => import(/* webpackChunkName: "about" */ './views/Newslist.vue')
+        }
+      ]
+    },
+    // {
+    //   path: '/news/list',
+    //   name: 'list',
+    //   // route level code-splitting
+    //   // this generates a separate chunk (about.[hash].js) for this route
+    //   // which is lazy-loaded when the route is visited.
+    //   component: () => import(/* webpackChunkName: "about" */ './views/Newslist.vue')
+    // }
+  ]
+})
---
+++ vue  router/views/News.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="about">
+    <h1>This is News</h1>
+    <router-link to="/news/list">List</router-link>
+    <router-view/>
+  </div>
+
+</template>
